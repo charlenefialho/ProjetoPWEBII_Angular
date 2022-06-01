76c2bb46489f23b6ed6d9e31656133b4bde6b87c
charlenefialho <fialhocharlene@gmail.com>
2022-05-31 22:14:59 -0300

    [Aula 22]Array,Objetos,ngFor no angular

--- PWEBII/frontend/src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ PWEBII/frontend/src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -6,8 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-produtos.component.css']
 })
 export class ListarProdutosComponent implements OnInit {
+  listaStrings: string[] = ['Primeiro','segundo','Terceiro'];
+  listaNumeros: number[] = [15,15.18,100];
 
-  constructor() { }
+  objetoModelo = {
+    nome: 'Charlene',
+    idade: 17,
+    altura: 1.65,
+    graduado: true
+  };
+
+  listaProdutos: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2021-12-31', id: 1},
+    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31', id: 2,promocao:true},
+    { id: 3,nome: 'Curso de Ionic', precoProduto: 50, validade: '2021-12-31'},
+  ];
+
+
+
+  constructor() {
+    for(let item of this.listaStrings){
+       console.log (item);
+    }
+
+    for(const item of this.listaNumeros){
+        console.log(item);
+    }
+
+    console.log(this.objetoModelo);
+  }
 
   ngOnInit(): void {
   }
